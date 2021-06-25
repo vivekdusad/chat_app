0e15e7e4f0861e66831760259a8dcffeb077f212
flutterStak <vivekdusad55@gmail.com>
2021-06-25 10:27:07 +0530

    first commit

---
+++ app/src/main/java/com/example/chat_app/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.chat_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editText = findViewById(R.id.editText);
+        findViewById(R.id.enterButton).setOnClickListener(view -> {
+            Intent intent;
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/chat_app/MessageAdapter.java
@@ -0,0 +1,4 @@
+package com.example.chat_app;
+
+public class MessageAdapter {
+}
